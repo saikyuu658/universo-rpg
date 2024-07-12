7884f2ee94c43209b27811100c09d1e5ab8f2dd2
David Pontes <145456877+saikyuu658@users.noreply.github.com>
2024-07-12 08:24:44 -0300

    tools page

---
+++ src/pages/Tools/index.tsx
@@ -0,0 +1,12 @@
+
+export default function Tools() {
+  return (
+    <div className="flex gap-10 min-h-[100vh] justify-center flex-col items-center align-middle">
+      <h1 className=" text-5xl text-orange-700">Hello!</h1>
+      <p>Sorry, but we're still undergoing maintenance here.</p>
+      <p className="text-orange-700">
+        Come back later!
+      </p>
+    </div>
+  );
+}
