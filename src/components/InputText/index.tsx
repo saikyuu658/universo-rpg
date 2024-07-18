006e101ac87a859d90c6a2fac45dbe3036c0c23d
David Pontes <145456877+saikyuu658@users.noreply.github.com>
2024-07-18 16:25:16 -0300

    Update input text

--- src/components/InputText/index.tsx
+++ src/components/InputText/index.tsx
@@ -1,6 +1,6 @@
 export default function InputText({icon, label, ...otherProps}: any){
     return (
-        <>
+        <div>
             <label htmlFor="email-input" className="text-sm" >{label}</label>
             <div id="email-input" className="flex items-center rounded-full mt-1 overflow-hidden bg-white text-orange-main py-1 px-2">
                 <span className="material-symbols-outlined text-md	">
@@ -8,6 +8,6 @@ export default function InputText({icon, label, ...otherProps}: any){
                 </span>
                 <input type="text" {...otherProps} className="outline-none w-full text-blue-deep-2  pl-1" />
             </div>
-        </>
+        </div>
     )
 }
